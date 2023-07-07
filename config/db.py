2f4cd5cf9733626393d85c53ed9d7550735b1e49
canos13 <canomsergio13@gamil.com>
2023-07-07 07:37:19 -0600

    var db

--- config/db.py
+++ config/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:@localhost:3306/news")
+#engine = create_engine("mysql+pymysql://root:@localhost:3306/news")
+engine = create_engine("mysql+pymysql://rx49pld6m8rwe7z2qe83:pscale_pw_8dJeUKWkuFUJVL3LK0I9yimZE9leMe3HQmVKZfdfxms@aws.connect.psdb.cloud:3306/news")
 meta = MetaData()
 conn = engine.connect()
