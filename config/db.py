d5fd775cabe79e8da7553c1604e66b7d15f186ef
canos13 <canomsergio13@gamil.com>
2023-07-07 07:58:27 -0600

    bd vars

--- config/db.py
+++ config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
 #engine = create_engine("mysql+pymysql://root:@localhost:3306/news")
-engine = create_engine("mysql+pymysql://rx49pld6m8rwe7z2qe83:pscale_pw_8dJeUKWkuFUJVL3LK0I9yimZE9leMe3HQmVKZfdfxms@aws.connect.psdb.cloud:3306/news")
+engine = create_engine("mysql+pymysql://uuxbcszkci1q50hq:HSWqLkRqGOKKTrC47Gj5@by3tkwb22zo3nffnstse-mysql.services.clever-cloud.com:3306/by3tkwb22zo3nffnstse")
 meta = MetaData()
 conn = engine.connect()
